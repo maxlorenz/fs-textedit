1fe49c47a9a5a67245a42225eeba8456d9583f05
Max Lorenz <max-lorenz@gmx.net>
2015-06-15 16:39:24 +0200

    Refactoring

--- WPF_POC/Program.fs
+++ WPF_POC/Program.fs
@@ -2,62 +2,56 @@
 
 open System
 open System.IO
-open System.Resources
-open System.Reflection
 open System.Windows
 open System.Windows.Controls
 open System.Windows.Markup
-open System.Xml
 open Microsoft.Win32
 
-let NO_FILE = String.Empty
-let mutable currentFile = NO_FILE
+type LastFile = None | Path of string
+let mutable currentFile = None
 
-let getWindow xaml =
+let getPath file =
+    match file with
+        | Path path -> path
+        | None      -> String.Empty
 
-    let assembly = Assembly.GetExecutingAssembly()
-    let resource = assembly.GetManifestResourceStream(xaml)
-    let stream = new StreamReader(resource)
-    let text = stream.ReadToEnd()
+let filter = "Textdokumente (.txt)|*.txt|Alle Dateien|*.*"
+let dialog_open = new OpenFileDialog(DefaultExt = ".txt", Filter = filter)
+let dialog_save = new SaveFileDialog(DefaultExt = ".txt", Filter = filter)
 
-    XamlReader.Parse(text) :?> Window
+let saveFile (txt: TextBox) path =
+    File.WriteAllText(path, txt.Text)
+    currentFile <- Path path
 
-let dialog_open = new OpenFileDialog(DefaultExt = ".txt", Filter = "Textdokumente (.txt)|*.txt")
-let dialog_save = new SaveFileDialog(DefaultExt = ".txt", Filter = "Textdokumente (.txt)|*.txt")
-
-let saveFile (txt: TextBox) =
-    File.WriteAllText(currentFile, txt.Text)
+let openFile (txt: TextBox) path =
+    txt.Text <- File.ReadAllText(path)
+    currentFile <- Path path
 
 let openFileWithDialog (txt: TextBox) = 
-    if dialog_open.ShowDialog().Value then
-        txt.Text <- File.ReadAllText(dialog_open.FileName)
-        currentFile <- dialog_open.FileName
+    if dialog_open.ShowDialog().Value then dialog_open.FileName |> openFile txt
 
 let saveFileWithDialog (txt: TextBox) =
-    if dialog_save.ShowDialog().Value then 
-        currentFile <- dialog_save.FileName
-        saveFile txt
+    if dialog_save.ShowDialog().Value then dialog_save.FileName |> saveFile txt
 
-let Window =
-    let temp = getWindow "Window1.xaml"
+let Editor =
+    let window = XAML.createFromRessource "Editor.xaml"
 
-    let btnNew  = temp.FindName("btnNew")  :?> MenuItem
-    let btnOpen = temp.FindName("btnOpen") :?> MenuItem
-    let btnSave = temp.FindName("btnSave") :?> MenuItem
-    let btnExit = temp.FindName("btnExit") :?> MenuItem
+    let btnNew    = window.FindName("btnNew")    :?> MenuItem
+    let btnOpen   = window.FindName("btnOpen")   :?> MenuItem
+    let btnSave   = window.FindName("btnSave")   :?> MenuItem
+    let btnSaveAs = window.FindName("btnSaveAs") :?> MenuItem
+    let btnExit   = window.FindName("btnExit")   :?> MenuItem
+    let txtMain   = window.FindName("txtMain")   :?> TextBox
 
-    let txtMain = temp.FindName("txtMain") :?> TextBox
-
-    btnNew.Click.Add (fun _ -> txtMain.Text <- String.Empty; currentFile <- NO_FILE)
+    btnNew.Click.Add (fun _ -> txtMain.Text <- String.Empty; currentFile <- None)
     btnOpen.Click.Add(fun _ -> openFileWithDialog txtMain)
-    btnSave.Click.Add(fun _ -> if currentFile = NO_FILE then saveFileWithDialog txtMain else saveFile txtMain)
-    btnExit.Click.Add(fun _ -> temp.Close())
+    btnSave.Click.Add(fun _ -> match currentFile with 
+                                | None      -> saveFileWithDialog txtMain 
+                                | Path path -> saveFile txtMain path)
+    btnSaveAs.Click.Add(fun _ -> saveFileWithDialog txtMain)
+    btnExit.Click.Add(fun _ -> window.Close())
 
-    temp
+    window
 
-let app = new Application()
-
-let main = app.Run(Window) |> ignore
-
-[<STAThread>]
-do main
+[<EntryPoint>] [<STAThread>]
+let main(_) = (new Application()).Run(Editor)
---
+++ WPF_POC/XAML.fs
@@ -0,0 +1,18 @@
+﻿module XAML
+
+open System.IO
+open System.Reflection
+open System.Resources
+open System.Windows
+open System.Windows.Markup
+open System.Xaml
+
+let createFromRessource file =
+
+    let assembly = Assembly.GetExecutingAssembly()
+    let resource = assembly.GetManifestResourceStream(file)
+    let stream = new StreamReader(resource)
+    let text = stream.ReadToEnd()
+
+    XamlReader.Parse(text) :?> Window
+
